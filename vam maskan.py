a3324ad626a18cc375746dca651fafc63e785302
jabbar <abduljabbar.raeisi1998@mail.com>
2024-07-22 08:48:50 +0330

    Complite project

--- vam maskan.py
+++ vam maskan.py
@@ -1,19 +1,13 @@
-import os
 import time
-import base64
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-import tensorflow as tf
-import subprocess
-import sys
-from selenium.common.exceptions import NoAlertPresentException, TimeoutException
-import re
+from selenium.common.exceptions import NoAlertPresentException, TimeoutException, NoSuchElementException, \
+    ElementClickInterceptedException, StaleElementReferenceException
 
 
-class Vam():
+class Vam:
     def __init__(self):
         options = webdriver.ChromeOptions()
         options.add_argument('--no-sandbox')
@@ -21,64 +15,115 @@ class Vam():
         self.driver = webdriver.Chrome(options=options)
         self.driver.get("https://saman.mrud.ir/")
 
-    def entery_form(self):
-        WebDriverWait(self.driver, 180).until(
-            EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/main/div/div/div[2]/a[1]'))).click()
-        WebDriverWait(self.driver, 180).until(EC.presence_of_element_located(
-            (By.XPATH, '/html/body/div/div/div/main/div/div/div/div[1]/div/div/a'))).click()
-        WebDriverWait(self.driver, 180).until(EC.presence_of_element_located((By.ID, 'username'))).send_keys(
-            "09307212580")
-        time.sleep(3)
-        WebDriverWait(self.driver, 180).until(EC.presence_of_element_located((By.ID, 'send-otp-form-btn'))).click()
-        time.sleep(8)
-        WebDriverWait(self.driver, 180).until(
-            EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/main/div/div/div[2]/a[1]'))).click()
-        WebDriverWait(self.driver, 180).until(
-            EC.presence_of_element_located(
-                (By.XPATH, '/html/body/div/div/div/main/div/div/div/div/div[1]/div/div[2]/div/div[3]'))).click()
-        WebDriverWait(self.driver, 180).until(
-            EC.presence_of_element_located((By.XPATH,
-                                            '/html/body/div/div/div/main/div/div/div/div/div[2]/div/div[2]/div/div[1]/a/div[2]/a'))).click()
-        WebDriverWait(self.driver, 180).until(
-            EC.presence_of_element_located(
-                (By.XPATH, '/html/body/div/div/div/main/div/div/div/div/div[2]/a[2]'))).click()
+    def wait_for_element(self, by, value, timeout=180):
         while True:
-            self.select_option_by_text(
-                "/html/body/div/div/div[2]/div/div/div[2]/form/div[3]/div/div/div/div[1]/div[1]/div/div/div[1]/div[1]/div[1]",
-                "سپه")
-            dropdown = Select(
-                WebDriverWait(self.driver, 180).until(EC.presence_of_element_located(
-                    (By.XPATH, '/html/body/div/div/div[2]/div/div/div[2]/form/div[3]/div/div/div/div[1]/div[2]/div'))))
-            if len(dropdown.options) > 1:
-                dropdown.select_by_index(1)
-                WebDriverWait(self.driver, 180).until(
-                    EC.presence_of_element_located(
-                        (By.XPATH, '/html/body/div/div/div[2]/div/div/div[3]/button'))).click()
-                break
-            else:
+            try:
+                element = WebDriverWait(self.driver, timeout).until(
+                    EC.presence_of_element_located((by, value))
+                )
+                return element
+            except (TimeoutException, NoSuchElementException, StaleElementReferenceException):
+                print(f"Retrying to find element {value}")
+                continue
+
+    def click_element(self, by, value, timeout=180):
+        while True:
+            try:
+                element = self.wait_for_element(by, value, timeout)
+                element.click()
+                return
+            except (
+                    ElementClickInterceptedException, TimeoutException, NoSuchElementException,
+                    StaleElementReferenceException):
+                print(f"Retrying to click element {value}")
+                time.sleep(1)
+                continue
+
+    def element_exists(self, by, value, timeout=10):
+        try:
+            WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((by, value)))
+            return True
+        except TimeoutException:
+            return False
+
+    def find_element_by_text(self, tag_name, text, timeout=10):
+        try:
+            element = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located((By.XPATH, f"//{tag_name}[contains(text(), '{text}')]"))
+            )
+            return element
+        except TimeoutException:
+            return None
+    def find_element_by_class_and_text(self, class_name, text, timeout=10):
+        try:
+            elements = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, class_name))
+            )
+            for element in elements:
+                if text in element.text:
+                    return element
+            return None
+        except TimeoutException:
+            return None
+
+    def entery_form(self):
+        self.click_element(By.XPATH, '/html/body/div/div/div/main/div/div/div[2]/a[1]')
+        self.click_element(By.XPATH, '/html/body/div/div/div/main/div/div/div/div[1]/div/div/a')
+        username_field = self.wait_for_element(By.ID, 'username')
+        username_field.send_keys("09307212580")
+        time.sleep(3)
+        self.click_element(By.ID, 'send-otp-form-btn')
+        time.sleep(8)
+        self.click_element(By.XPATH, '/html/body/div/div/div/main/div/div/div[2]/a[1]')
+        self.click_element(By.XPATH, '/html/body/div/div/div/main/div/div/div/div/div[1]/div/div[2]/div/div[3]')
+        time.sleep(3)
+        self.click_element(By.XPATH, '//*[@id="app"]/div/main/div/div/div/div/div[2]/div/div[2]/div/div[1]/a/div[2]/a')
+        # self.click_element(By.XPATH, '//*[@id="app"]/div/main/div/div/div/div/div[2]/a[2]')
+        while True:
+            try:
+                # کلیک بر روی لینک
+                self.click_element(By.XPATH, '//*[@id="app"]/div/main/div/div/div/div/div[2]/a[2]')
+                time.sleep(1)
+
+                # کلیک بر روی عنصر فرم
+                self.click_element(By.XPATH,
+                                   '//*[@id="app"]/div[2]/div/div/div[2]/form/div[3]/div/div/div/div[1]/div[1]/div/div/div[1]/div[1]/div[2]/div')
+                time.sleep(5)
+
+                # پیدا کردن و کلیک بر روی آیتم لیست
+                wait = WebDriverWait(self.driver, 10)
+                element = wait.until(EC.presence_of_element_located((By.XPATH, "//div[contains(text(), 'سپه')]")))
+
+                # اسکرول به عنصر
+                self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
+                element = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'سپه')]")))
+                element.click()
+                time.sleep(3)
+
+                # بررسی وجود عنصر مورد نظر
+                xpath_to_check = '//*[@id="app"]/div[2]/div/div/div[2]/form/div[3]/div/div/div/div[2]/div/div'
+                if self.element_exists(By.XPATH, xpath_to_check):
+                    print("Bank Close !!!!!!!!!! ")
+                    self.driver.refresh()
+                    time.sleep(5)
+                    continue
+                else:
+                    print(" !!!!!!!!!!!  Bank Open  !!!!!!!!!! ")
+                    time.sleep(50)
+                    break
+            except Exception as e:
+                print(f"An error occurred: {e}")
                 self.driver.refresh()
-                WebDriverWait(self.driver, 180).until(
-                    EC.presence_of_element_located(
-                        (By.XPATH, '/html/body/div/div/div/main/div/div/div/div/div[2]/a[2]'))).click()
+                time.sleep(5)
+                continue
 
-    def select_option_by_text(self, element_xpath, option_text):
+    def check_class_exists(self, class_name):
         try:
-            dropdown = Select(
-                WebDriverWait(self.driver, 180).until(EC.presence_of_element_located((By.XPATH, element_xpath))))
-            dropdown.select_by_visible_text(option_text)
+            elements = self.driver.find_elements(By.CSS_SELECTOR, f".{class_name.replace(' ', '.')}")
+            return len(elements) > 0
         except Exception as e:
-            print(f"Error selecting option: {e}")
-
-    def select_second_option(self, element_xpath):
-        try:
-            dropdown = Select(
-                WebDriverWait(self.driver, 180).until(EC.presence_of_element_located((By.XPATH, element_xpath))))
-            if len(dropdown.options) > 1:
-                dropdown.select_by_index(1)
-            else:
-                print(f"We didn't select a second option")
-        except Exception as e:
-            print(f"Error selecting second option: {e}")
+            print(f"Error checking class: {e}")
+            return False
 
 
 vam = Vam()
